024d9f8dce6fe70d921475b5c39737bc32fd8aed
peedee2k <peedee_2k@yahoo.com>
2018-01-18 09:30:16 -0600

    Initial Commit

---
+++ LoginApp With Brian/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginApp With Brian
+//
+//  Created by Pankaj Sharma on 1/18/18.
+//  Copyright © 2018 Pankaj Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
